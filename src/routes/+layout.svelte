642ab4491e073549a41c8ae8d37931c77827ce1b
Amine13u <46264764+Amine13u@users.noreply.github.com>
2022-09-21 17:43:25 +0100

    feat: Adding Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
+<nav class="px-10 py-3 bg-purple-800 text-white flex justify-between items-center">
+	<a href="/" class="text-xl">AppIcon</a>
+	<a href="/sign-in" class="font-bold"> Sign In</a>
+</nav>
+
 <slot />
